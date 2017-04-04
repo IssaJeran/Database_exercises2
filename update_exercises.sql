f2ea4378ab55ce0ae4dd7c21ae16739e601016d2
IssaJeran <jeransmith22@gmail.com>
2017-04-04 12:12:24 -0500

    small update

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,10 +6,10 @@ SELECT 'ALL ALBUMS' AS 'INFO';
 SELECT * FROM albums \G;
 
 SELECT 'ALL ALBUMS BEFORE 1980' AS 'INFO';
-SELECT name FROM albums WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1980;
 UPDATE albums SET release_date = (release_date - 100) WHERE release_date < 1980;
 SELECT 'ALL ALBUMS IN 1800s' AS 'INFO';
-SELECT name FROM albums WHERE release_date < 1900;
+SELECT * FROM albums WHERE release_date < 1900;
 
 SELECT 'ALL MICHAEL JACKSON ALBUMS' AS 'INFO';
 SELECT name FROM albums WHERE artist = "Michael Jackson";
